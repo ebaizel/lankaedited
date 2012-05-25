c6fe2c8a6fb8dc302d689298fb564cb4a59a5b23
Emile Baizel <ebaizel@gmail.com>
2012-05-25 12:10:25 -0700

    added server.js file

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\nApp (lankaedited) is running on Node.JS ' + process.version);
+}).listen(process.env['app_port'] || 3000);
+
